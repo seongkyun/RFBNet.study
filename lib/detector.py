1a2a2f6e668b47b0290611f117e863c580128521
Seongkyun Han <skhan3410@naver.com>
2019-05-29 20:32:16 +0900

    demo fastening

--- lib/detector.py
+++ lib/detector.py
@@ -15,39 +15,33 @@ class ObjectDetector:
         self.transform = transform
         self.detector = detector
 
-    def predict(self, img, threshold=0.6, check_time=False):
+    def predict(self, img, threshold=0.6):
         # make sure the input channel is 3 
         assert img.shape[2] == 3
         #scale = torch.Tensor([img.shape[1::-1], img.shape[1::-1]])
         scale = torch.Tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
         
-        _t = {'preprocess': Timer(), 'net_forward': Timer(), 'detect': Timer(), 'output': Timer()}
+        _t = {'inference': Timer(), 'misc': Timer(), 'total': Timer()}
         
         # preprocess image
-        _t['preprocess'].tic()
+        _t['total'].tic()
         with torch.no_grad():
-            x = self.transform(img).unsqueeze(0).cuda()
+            #x = self.transform(img).unsqueeze(0)
+            x = self.transform(img).unsqueeze(0).cuda() # for fastening
             #if args.cuda:
             #    x = x.cuda()
             #    scale = scale.cuda()
-        #if self.use_gpu:
-        #    x = x.cuda()
-        #if self.half:
-        #    x = x.half()
-        preprocess_time = _t['preprocess'].toc()
 
         # forward
-        _t['net_forward'].tic()
+        _t['inference'].tic()
         out = self.model(x)  # forward pass
-        net_forward_time = _t['net_forward'].toc()
+        inference_time = _t['inference'].toc()
 
         # detect
-        _t['detect'].tic()
+        _t['misc'].tic()
         detections = self.detector.forward(out, self.priors)
-        detect_time = _t['detect'].toc()
         
         # output
-        _t['output'].tic()
         labels, scores, coords = [list() for _ in range(3)]
         batch=0
         for classes in range(detections.size(1)):
@@ -57,13 +51,10 @@ class ObjectDetector:
                 labels.append(classes-1)
                 coords.append(detections[batch,classes,num,1:]*scale)
                 num+=1
-        output_time = _t['output'].toc()
-        total_time = preprocess_time + net_forward_time + detect_time + output_time
+        misc_time = _t['misc'].toc()
+        total_time = _t['total'].toc()
         
-        if check_time is True:
-            return labels, scores, coords, (total_time, preprocess_time, net_forward_time, detect_time, output_time)
-            # total_time = preprocess_time + net_forward_time + detect_time + output_time
-            # print('total time: {} \n preprocess: {} \n net_forward: {} \n detect: {} \n output: {}'.format(
-            #     total_time, preprocess_time, net_forward_time, detect_time, output_time
-            # ))
-        return labels, scores, coords
+        #if check_time is True:
+        #    return labels, scores, coords, (total_time, inference_time, misc_time)
+        #return labels, scores, coords
+        return labels, scores, coords, (total_time, inference_time, misc_time)
