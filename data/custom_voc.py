a4a08267a391df5280b781c391f4f83c745ec615
Seongkyun Han <skhan3410@naver.com>
2019-06-14 15:19:38 +0900

    misc changes

--- data/custom_voc.py
+++ data/custom_voc.py
@@ -30,61 +30,6 @@ CLASSES = ( '__background__', # always index 0
 COLORS = ((255, 0, 0, 128), (0, 255, 0, 128), (0, 0, 255, 128),
           (0, 255, 255, 128), (255, 0, 255, 128), (255, 255, 0, 128))
 
-
-'''class VOCSegmentation(data.Dataset):
-
-    """VOC Segmentation Dataset Object
-    input and target are both images
-
-    NOTE: need to address https://github.com/pytorch/vision/issues/9
-
-    Arguments:
-        root (string): filepath to VOCdevkit folder.
-        image_set (string): imageset to use (eg: 'train', 'val', 'test').
-        transform (callable, optional): transformation to perform on the
-            input image
-        target_transform (callable, optional): transformation to perform on the
-            target image
-        dataset_name (string, optional): which dataset to load
-            (default: 'VOC2007')
-    """
-
-    def __init__(self, root, image_set, transform=None, target_transform=None,
-                 dataset_name='VOC2007'):
-        self.root = root
-        self.image_set = image_set
-        self.transform = transform
-        self.target_transform = target_transform
-
-        self._annopath = os.path.join(
-            self.root, dataset_name, 'SegmentationClass', '%s.png')
-        self._imgpath = os.path.join(
-            self.root, dataset_name, 'JPEGImages', '%s.jpg')
-        self._imgsetpath = os.path.join(
-            self.root, dataset_name, 'ImageSets', 'Segmentation', '%s.txt')
-
-        with open(self._imgsetpath % self.image_set) as f:
-            self.ids = f.readlines()
-        self.ids = [x.strip('\n') for x in self.ids]
-
-    def __getitem__(self, index):
-        img_id = self.ids[index]
-
-        target = Image.open(self._annopath % img_id).convert('RGB')
-        img = Image.open(self._imgpath % img_id).convert('RGB')
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return img, target
-
-    def __len__(self):
-        return len(self.ids)
-'''
-
 class AnnoTransform_custom(object):
 
     """Transforms a VOC annotation into a Tensor of bbox coords and label index
@@ -163,23 +108,9 @@ class CustomDetection(data.Dataset):
         self.name = dataset_name
         self._annopath = os.path.join('%s', 'annotations', '%s.xml')
         self._imgpath = os.path.join('%s', 'images', '%s.jpg')
-        #print(self.root) #/mnt/hdd640/drone_even_trainset
-        #print(self.image_set) #Customset
-        #print(self.preproc) # address
-        #print(self.target_transform) # address
-        #print(self.name) #images
-        #print(self._annopath) # %s/annotations/%s.xml
-        #print(self._imgpath) # %s/images/%s.jpg
         self.ids = list()
-        #for (year, name) in image_sets:
-        #    self._year = year
-        #    rootpath = os.path.join(self.root, 'VOC' + year)
-        #    for line in open(os.path.join(rootpath, 'ImageSets', 'Main', name + '.txt')):
-        #        self.ids.append((rootpath, line.strip()))
         for line in open(os.path.join(self.root, "trainval.txt")):
             self.ids.append((root, line.strip()))
-        #print(self.ids)
-        #sys.exit()
 
     def __getitem__(self, index):
         img_id = self.ids[index]
@@ -247,96 +178,6 @@ class CustomDetection(data.Dataset):
         '''
         to_tensor = transforms.ToTensor()
         return torch.Tensor(self.pull_image(index)).unsqueeze_(0)
-
-    '''
-    def evaluate_detections(self, all_boxes, output_dir=None):
-        """
-        all_boxes is a list of length number-of-classes.
-        Each list element is a list of length number-of-images.
-        Each of those list elements is either an empty list []
-        or a numpy array of detection.
-
-        all_boxes[class][image] = [] or np.array of shape #dets x 5
-        """
-        self._write_voc_results_file(all_boxes)
-        self._do_python_eval(output_dir)
-
-    def _get_voc_results_file_template(self):
-        filename = 'comp4_det_test' + '_{:s}.txt'
-        filedir = os.path.join(
-            self.root, 'results', 'VOC' + self._year, 'Main')
-        if not os.path.exists(filedir):
-            os.makedirs(filedir)
-        path = os.path.join(filedir, filename)
-        return path
-
-    def _write_voc_results_file(self, all_boxes):
-        for cls_ind, cls in enumerate(CLASSES):
-            cls_ind = cls_ind 
-            if cls == '__background__':
-                continue
-            print('Writing {} VOC results file'.format(cls))
-            filename = self._get_voc_results_file_template().format(cls)
-            with open(filename, 'wt') as f:
-                for im_ind, index in enumerate(self.ids):
-                    index = index[1]
-                    dets = all_boxes[cls_ind][im_ind]
-                    if dets == []:
-                        continue
-                    for k in range(dets.shape[0]):
-                        f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
-                                format(index, dets[k, -1],
-                                dets[k, 0] + 1, dets[k, 1] + 1,
-                                dets[k, 2] + 1, dets[k, 3] + 1))
-
-    def _do_python_eval(self, output_dir='output'):
-        rootpath = os.path.join(self.root, 'VOC' + self._year)
-        name = self.image_set[0][1]
-        annopath = os.path.join(
-                                rootpath,
-                                'Annotations',
-                                '{:s}.xml')
-        imagesetfile = os.path.join(
-                                rootpath,
-                                'ImageSets',
-                                'Main',
-                                name+'.txt')
-        cachedir = os.path.join(self.root, 'annotations_cache')
-        aps = []
-        # The PASCAL VOC metric changed in 2010
-        use_07_metric = True if int(self._year) < 2010 else False
-        print('VOC07 metric? ' + ('Yes' if use_07_metric else 'No'))
-        if output_dir is not None and not os.path.isdir(output_dir):
-            os.mkdir(output_dir)
-        for i, cls in enumerate(CLASSES):
-
-            if cls == '__background__':
-                continue
-
-            filename = self._get_voc_results_file_template().format(cls)
-            rec, prec, ap = voc_eval(
-                                    filename, annopath, imagesetfile, cls, cachedir, ovthresh=0.5,
-                                    use_07_metric=use_07_metric)
-            aps += [ap]
-            print('AP for {} = {:.4f}'.format(cls, ap))
-            if output_dir is not None:
-                with open(os.path.join(output_dir, cls + '_pr.pkl'), 'wb') as f:
-                    pickle.dump({'rec': rec, 'prec': prec, 'ap': ap}, f)
-        print('Mean AP = {:.4f}'.format(np.mean(aps)))
-        print('~~~~~~~~')
-        print('Results:')
-        for ap in aps:
-            print('{:.3f}'.format(ap))
-        print('{:.3f}'.format(np.mean(aps)))
-        print('~~~~~~~~')
-        print('')
-        print('--------------------------------------------------------------')
-        print('Results computed with the **unofficial** Python eval code.')
-        print('Results should be very close to the official MATLAB eval code.')
-        print('Recompute with `./tools/reval.py --matlab ...` for your paper.')
-        print('-- Thanks, The Management')
-        print('--------------------------------------------------------------')
-    '''
 
 def detection_collate(batch):
     """Custom collate fn for dealing with batches of images that have a different
